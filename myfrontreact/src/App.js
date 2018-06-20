5452c2d3866108ba846069e693d4a632a2886adc
Juan Carlos <h4rry@Juans-MacBook-Pro.local>
2018-06-19 20:33:00 -0500

    on input change, edit values on html

--- myfrontreact/src/App.js
+++ myfrontreact/src/App.js
@@ -4,11 +4,47 @@ import './App.css';
 import MyFirstComponent from './components/myfirstcomponent';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      firstBox: "first box default",
+      secondBox: "second box default",
+      thirdBOx: "third box default",
+      inputValue: "textoDefault"
+    }
+  };
+
+  handleClickOne = (event) => {
+      event.preventDefault();
+      this.setState({
+        firstBox: this.state.inputValue
+      })
+  }
+  handleClickTwo = (event) => {
+      event.preventDefault();
+      this.setState({
+        secondBox: this.state.inputValue
+      })
+
+  }
+  handleClickThree = (event) => {
+      event.preventDefault();
+      this.setState({
+        thirdBOx: this.state.inputValue
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <MyFirstComponent text={"Hola devG"}/>
-        <MyFirstComponent text={"ya se acabó"}/>
+          <input onChange= {(evt) => { this.state.inputValue=evt.target.value }} />
+          <button onClick={this.handleClickOne} >Cambiar 1</button>
+          <MyFirstComponent  text={this.state.firstBox}/>
+          <button onClick={this.handleClickTwo} >Cambiar 2</button>
+          <MyFirstComponent  text={this.state.secondBox}/>
+          <button onClick={this.handleClickThree} >Cambiar 3</button>
+          <MyFirstComponent  text={this.state.thirdBOx}/>
       </div>
     );
   }
